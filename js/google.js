3a4bf90a2fe8e79b55df4d92360c30b12a073ea9
sprunkix <incredibox@sprunkix.com>
2025-03-02 20:04:01 +0800

    sprunki oc game and unload google adsense resolve

--- js/google.js
+++ js/google.js
@@ -4,4 +4,4 @@ gtag('js', new Date());
 gtag('config', 'G-SWTJE96WXM');
 gtag('config', 'AW-1018808221');
 
-(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();
+//(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();
